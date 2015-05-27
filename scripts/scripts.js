074e9eec4ada11df63d34de840363aee94190630
kylejenkinsdmd <xdkylexd@hotmail.co.uk>
2015-05-27 19:20:01 +0100

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,22 +2,17 @@ $(document).ready(function() {
     
 //retrieve scroll point, mainly for triggering stuff at a specific point. Called everytime the user scrolls the page, in any way, shape or form. Can be seen on the inspect. 
     $(document).scroll(function(e){
-            var scrollPosition  =$("body").scrollTop(); //
-            console.log(scrollPosition );
+        var scrollPosition  =$("body").scrollTop(); //
+        console.log(scrollPosition );
         
-            
-            //if scroll position is greater than 600, then hide image.
-            if(scrollPosition   > 600) { 
-                $("#theImage").hide();
-                
-            }
-        //if scroll position is less than 200, then hide image.
-            if(scrollPosition  < 200) { 
-                $("#theImage").show();
-                
-            }
+        //if scroll position is greater than 3180, then move image up.
+        if(scrollPosition   > 3180) { 
+            $("#text").animate({top:"50px"}, "slow");
+        }
+        //if scroll position is less than 3190, then move image down.
+        if(scrollPosition  < 3190) { 
+            $("#text").animate({top:"150px"}, "slow");
+        }
+
     });
-
 });
-
-
