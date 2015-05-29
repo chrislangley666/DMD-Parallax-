663d89fa88be5c8f829a800c946245343a57d852
chrislangley666 <chrislangley666@hotmail.co.uk>
2015-05-29 16:59:22 +0100

    here it is ahaha

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -15,6 +15,34 @@ $(document).ready(function() {
                     $("#text").animate({top:"150px"}, "slow");
 
                 }
-
     });
+
+
+});
+
+$(document).ready(function() {
+    
+    /*
+    This hides the image that is shown later on scroll. Might be better to set 
+    up a css class for this and set the display: none; property. This might 
+    avoid seeing the content briefly before our script file loads and hides it. 
+    */
+    $("#dates").hide();     
+    
+    /*
+    Function that gets called every time the page is scrolled in either direction. 
+    We've set up a variable to store the scroll position. You can use conditional 
+    statements to fire off events based on that number. Here we're showing an image 
+    if were over 250 and hiding it again if the number is lower than that. 
+    */
+    $(document).scroll(function(e) {
+        var scrollPosition = $("body").scrollTop();
+        console.log(scrollPosition); 
+       
+        if(scrollPosition > 9067) {
+            $("#dates").fadeIn();
+        }
+  
+    });
+    
 });
